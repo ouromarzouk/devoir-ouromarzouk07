07ebbeb5a32b164e1ce4bbbb7ad10fe25c610be8
ouromarzouk <132060104+ouromarzouk@users.noreply.github.com>
2023-06-07 15:19:45 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from pydantic import BaseModel
-
+from re import match
 
 class Vehicle(BaseModel):
     registration: str
@@ -15,5 +15,30 @@ class BillItem(BaseModel):
     quotation: float
     surcharge: float
     total: float
-
+@app.get("/bills/{owner}")
+async def get_bill_for_owner(owner: str) -> Bill:
+    bill = next(bill for bill in bills if bill.owner == owner)
+    
+    # Calculate bill items
+    for vehicle in vehicles:
+        if vehicle.owner == owner:
+            surcharge = vehicle.quotation * 0.1  # 10% surcharge
+            total = vehicle.quotation + surcharge
+            item = BillItem(registration=vehicle.registration, quotation=vehicle.quotation, surcharge=surcharge, total=total)
+            bill.items.append(item)
+            bill.total += total
+    
+    return bill
+class Vehicle(BaseModel):
+    registration: str
+    category: str
+    owner: str
+    quotation: float
+    
+    @validator("registration")
+    def validate_registration(cls, value: str) -> str:
+        pattern = r"^[A-Z]{2}-\d{3}-[A-Z]{2}$"
+        if not match(pattern, value):
+            raise ValueError("Invalid registration number")
+        return value
 
